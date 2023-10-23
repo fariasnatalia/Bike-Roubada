12d34e47a14af7816d43a50efdbd972a3110378c
natalia Farias Santos <102639784+fariasnatalia@users.noreply.github.com>
2023-10-23 17:49:10 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,63 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState }  from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-web';
+
+export default function App() {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  
+  function handleSignIn() {
+    const data  = {
+      username,
+      email,
+      password
+    }
+    console.log(data);
+
+  }
+  
+  return (
+    <View style={styles.container}>
+     
+      <Text>Bem-Vindo!</Text>
+      
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+        placeholder="Digite seu  nome:"
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Digite seu email"
+      />    
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Digite sua senha"
+        secureTextEntry={True}
+      />    
+
+      <TouchableOpacity style={styles.button} onPress={handleSignIn}> 
+        <Text style={styles.buttonText}>Acessar</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,28 @@
+Instalando o NativeBase :
+
+``` npm install native-base```
+ou 
+``` yarn add native-base  ```
+
+Instalando React-Native
+
+```npx expo install react-native-web@~0.19.6 react-dom@18.2.0```
+ou
+```nvm expo install react-native-web@~0.19.6 react-dom@18.2.0``` 
+
+```yarn expo install react-native-web@~0.19.6 react-dom@18.2.0```
+
+[Link para instalação](https://reactnative.dev/docs/alert)
+
+Outras instalações ultilizadas O NativeBase
+servirá apenas para cuidar do Layout da aplicação, nada mais é do que as estilizações em cima de componentes.
+
+``` yarn add native-base ``````
+
+Rodando o projeto no navegador:
+
+```npx expo start```
+
+BUGS: 
+- [] - footer
+-  [] - header
